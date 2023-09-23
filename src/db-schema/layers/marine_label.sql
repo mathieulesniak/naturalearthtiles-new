66b422a29c3dcbbaec4c0c0f0e37a4cfb9bb1cd0
Mathieu LESNIAK <mathieu@lesniak.fr>
2023-09-23 12:11:23 +0200

    create

--- src/db-schema/layers/marine_label.sql
+++ src/db-schema/layers/marine_label.sql
@@ -1,17 +1,17 @@
 DROP VIEW marine_label_z2;
-CREATE OR REPLACE VIEW marine_label_z2 AS (
+CREATE VIEW marine_label_z2 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_110m_geography_marine_polys
 );
 
 DROP VIEW marine_label_z5;
-CREATE OR REPLACE VIEW marine_label_z5 AS (
+CREATE VIEW marine_label_z5 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_50m_geography_marine_polys
 );
 
 DROP VIEW marine_label_z7;
-CREATE OR REPLACE VIEW marine_label_z7 AS (
+CREATE VIEW marine_label_z7 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_10m_geography_marine_polys
 );
