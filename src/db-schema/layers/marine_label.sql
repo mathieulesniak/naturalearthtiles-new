8c07d7fa5c53567057b2d0b7ab2d6af810103c02
Mathieu LESNIAK <mathieu@lesniak.fr>
2023-09-23 12:08:45 +0200

    drops

--- src/db-schema/layers/marine_label.sql
+++ src/db-schema/layers/marine_label.sql
@@ -1,13 +1,16 @@
+DROP VIEW marine_label_z2;
 CREATE OR REPLACE VIEW marine_label_z2 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_110m_geography_marine_polys
 );
 
+DROP VIEW marine_label_z5;
 CREATE OR REPLACE VIEW marine_label_z5 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_50m_geography_marine_polys
 );
 
+DROP VIEW marine_label_z7;
 CREATE OR REPLACE VIEW marine_label_z7 AS (
     SELECT geom, name, name_fr, scalerank, featurecla AS type
     FROM ne_10m_geography_marine_polys
--- src/db-schema/layers/state_label.sql
+++ src/db-schema/layers/state_label.sql
@@ -11,17 +11,22 @@ $$ LANGUAGE plpgsql IMMUTABLE;
 
 -- UPDATE ne_50m_admin_1_states_provinces
 -- SET name=fix_win1252_shp_encoding(name);
+DROP VIEW state_label_z3;
 
 CREATE OR REPLACE VIEW state_label_z3 AS (
     SELECT geom, name, name_fr scalerank, labelrank, postal
     FROM ne_50m_admin_1_states_provinces
 );
 
+DROP VIEW state_label_z5;
+
 CREATE OR REPLACE VIEW state_label_z5 AS (
     SELECT geom, name, name_fr scalerank, labelrank, postal
     FROM ne_10m_admin_1_states_provinces
     WHERE scalerank <= 6
 );
+
+DROP VIEW state_label_z7;
 
 CREATE OR REPLACE VIEW state_label_z7 AS (
     SELECT geom, name, name_fr, scalerank, labelrank, postal
